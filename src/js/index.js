f5d5ecdfd77f6712b69c2d703c20837541dafa61
Igor Yatsenko <igor.yatsenko09@gmail.com>
2023-05-27 19:37:43 +0300

    Update v.02.01

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const apiKey = '36697447-2a4d8e47928b599bac08c066e';
 const perPage = 40;
@@ -24,7 +26,7 @@ async function searchImages(query, page) {
 function renderImages(images) {
   const galleryElement = document.querySelector('.gallery');
 
-  images.forEach((image) => {
+  images.forEach(image => {
     const photoCard = document.createElement('div');
     photoCard.classList.add('photo-card');
 
@@ -33,18 +35,45 @@ function renderImages(images) {
     imgElement.alt = image.tags;
     imgElement.loading = 'lazy';
 
+    const aElement = document.createElement('a');
+    aElement.href = image.largeImageURL;
+    aElement.setAttribute('data-lightbox', 'gallery');
+
     const infoElement = document.createElement('div');
     infoElement.classList.add('info');
-    infoElement.innerHTML = `<p class="info-item"><b>Likes:</b> ${image.likes}</p>
-                             <p class="info-item"><b>Views:</b> ${image.views}</p>
-                             <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-                             <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>`;
+    infoElement.innerHTML = `<p class="info-item"><b>Likes</b> ${image.likes}</p>
+                             <p class="info-item"><b>Views</b> ${image.views}</p>
+                             <p class="info-item"><b>Comments</b> ${image.comments}</p>
+                             <p class="info-item"><b>Downloads</b> ${image.downloads}</p>`;
 
-    photoCard.appendChild(imgElement);
+    aElement.appendChild(imgElement);
+    photoCard.appendChild(aElement);
     photoCard.appendChild(infoElement);
 
     galleryElement.appendChild(photoCard);
+
+    function playNextImageGroup() {
+      // Виконати логіку відтворення наступної групи зображень
+    
+      const { height: cardHeight } = document
+        .querySelector(".gallery")
+        .firstElementChild.getBoundingClientRect();
+    
+      window.scrollBy({
+        top: cardHeight / 2,
+        behavior: "smooth",
+      });
+    }
+    
+    // Викликати функцію після відтворення кожної наступної групи зображень
+    playNextImageGroup();
+    
+
   });
+
+  // Оновлення галереї після додавання нових зображень
+  const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
 }
 
 // Функція для очищення галереї
@@ -58,87 +87,95 @@ function showEndOfResultsMessage() {
   const loadMoreButton = document.querySelector('.load-more');
   loadMoreButton.style.display = 'none';
 
-  Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+  Notiflix.Notify.info(
+    "We're sorry, but you've reached the end of search results."
+  );
 }
 
 // Функція для відображення повідомлення про кількість знайдених зображень
 function showTotalHitsMessage(totalHits) {
-    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-  }
-  
+  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+}
 
 // Обробник події для форми пошуку
-document.getElementById('search-form').addEventListener('submit', async (event) => {
-  event.preventDefault();
+document
+  .getElementById('search-form')
+  .addEventListener('submit', async event => {
+    event.preventDefault();
 
-  const searchQuery = document.querySelector('input[name="searchQuery"]').value;
+    const searchQuery = document.querySelector(
+      'input[name="searchQuery"]'
+    ).value;
 
-  try {
-    clearGallery();
+    try {
+      clearGallery();
 
-    const { hits, totalHits } = await searchImages(searchQuery, 1);
+      const { hits, totalHits } = await searchImages(searchQuery, 1);
 
-    if (hits.length > 0) {
-      renderImages(hits);
+      if (hits.length > 0) {
+        renderImages(hits);
 
-      // Перевірка наявності наступних результатів для пагінації
-      if (totalHits > perPage) {
-        showLoadMoreButton();
-        currentPage = 1;
+        // Перевірка наявності наступних результатів для пагінації
+        if (totalHits > perPage) {
+          showLoadMoreButton();
+          currentPage = 1;
+        } else {
+          hideLoadMoreButton();
+        }
+
+        showTotalHitsMessage(totalHits);
       } else {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
         hideLoadMoreButton();
       }
-
-      showTotalHitsMessage(totalHits);
-    } else {
-      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    } catch (error) {
+      Notiflix.Notify.failure('Error fetching images. Please try again later.');
       hideLoadMoreButton();
     }
-  } catch (error) {
-    Notiflix.Notify.failure('Error fetching images. Please try again later.');
-    hideLoadMoreButton();
-  }
-    });
-    
-    const heroTitle = document.querySelector('.hero-title');
-    const searchContainer = document.querySelector('.search-container');
-    heroTitle.classList.add('search-active');
-    searchContainer.classList.add('search-active');
+  });
+
+const heroTitle = document.querySelector('.hero-title');
+const searchContainer = document.querySelector('.search-container');
+heroTitle.classList.add('search-active');
+searchContainer.classList.add('search-active');
 
 // Функція для відображення кнопки "Load more"
 function showLoadMoreButton() {
-    const loadMoreButton = document.querySelector('.load-more');
-    loadMoreButton.style.display = 'block';
-  }
-  
-  // Функція для приховування кнопки "Load more"
-  function hideLoadMoreButton() {
-    const loadMoreButton = document.querySelector('.load-more');
-    loadMoreButton.style.display = 'none';
-  }
-  
-  // Приховання кнопки "Load more" при запуску
-  hideLoadMoreButton();
-  
+  const loadMoreButton = document.querySelector('.load-more');
+  loadMoreButton.style.display = 'block';
+}
 
-    // Обробник події для кнопки "Load more"
-    document.querySelector('.load-more').addEventListener('click', async () => {
-    const searchQuery = document.querySelector('input[name="searchQuery"]').value;
-    
-    try {
-    const { hits, totalHits } = await searchImages(searchQuery, currentPage + 1);
+// Функція для приховування кнопки "Load more"
+function hideLoadMoreButton() {
+  const loadMoreButton = document.querySelector('.load-more');
+  loadMoreButton.style.display = 'none';
+}
+
+// Приховання кнопки "Load more" при запуску
+hideLoadMoreButton();
+
+// Обробник події для кнопки "Load more"
+document.querySelector('.load-more').addEventListener('click', async () => {
+  const searchQuery = document.querySelector('input[name="searchQuery"]').value;
+
+  try {
+    const { hits, totalHits } = await searchImages(
+      searchQuery,
+      currentPage + 1
+    );
     if (hits.length > 0) {
-        renderImages(hits);
-        currentPage++;
-      
-        // Перевірка наявності наступних результатів для пагінації
-        if (currentPage * perPage >= totalHits) {
-          showEndOfResultsMessage();
-        }
-      } else {
+      renderImages(hits);
+      currentPage++;
+
+      if (currentPage * perPage >= totalHits) {
         showEndOfResultsMessage();
       }
-    } catch (error) {
-        Notiflix.Notify.failure('Error fetching images. Please try again later.');
-        }
-        });      
+    } else {
+      showEndOfResultsMessage();
+    }
+  } catch (error) {
+    Notiflix.Notify.failure('Error fetching images. Please try again later.');
+  }
+});
